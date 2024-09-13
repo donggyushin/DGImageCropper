7eeceba9a3915f970977e4823e3c42e3f688ccb9
Loopy <loopy@heyratel.com>
2024-09-13 09:26:29 +0900

    Chore

--- Sources/DGImageCropper/DGImageCropper.swift
+++ Sources/DGImageCropper/DGImageCropper.swift
@@ -1,2 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct DGImageCropper: View {
+    
+    let uiImage: UIImage
+    
+    public init(uiImage: UIImage) {
+        self.uiImage = uiImage
+    }
+    
+    public var body: some View {
+        Text("Text")
+    }
+}
+
+#Preview {
+    
+    let url = Bundle.module.path(forResource: "sample_image", ofType: "png")!
+    
+    return DGImageCropper(uiImage: .init(contentsOfFile: url)!)
+        .preferredColorScheme(.dark)
+}
