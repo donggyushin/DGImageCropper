7ca36c735d2ab84a619268264879c6efb56bb337
Loopy <loopy@heyratel.com>
2024-09-13 11:49:04 +0900

    Check Draggable

--- Sources/DGImageCropper/Model.swift
+++ Sources/DGImageCropper/Model.swift
@@ -76,14 +76,26 @@ public final class ImageCropperModel: ObservableObject {
             }
         case .bottomLeadingPoint:
             let value = (size.width - size.height) / 2
-            bottomLeadingPoint = CGPoint(x: previousBottomLeadingPoint.x + value, y: previousBottomLeadingPoint.y - value)
-            topLeadingPoint = CGPoint(x: previousTopLeadingPoint.x + value, y: previousTopLeadingPoint.y)
-            bottomTrailingPoint = CGPoint(x: previousBottomTrailingPoint.x, y: previousBottomTrailingPoint.y - value)
+            let bottomLeadingPoint = CGPoint(x: previousBottomLeadingPoint.x + value, y: previousBottomLeadingPoint.y - value)
+            let topLeadingPoint = CGPoint(x: previousTopLeadingPoint.x + value, y: previousTopLeadingPoint.y)
+            let bottomTrailingPoint = CGPoint(x: previousBottomTrailingPoint.x, y: previousBottomTrailingPoint.y - value)
+            
+            if CheckDraggableUseCase(point1: bottomLeadingPoint, point2: topTrailingPoint, minDiff: minDiff, originRect: originRect).execute() {
+                self.bottomLeadingPoint = bottomLeadingPoint
+                self.topLeadingPoint = topLeadingPoint
+                self.bottomTrailingPoint = bottomTrailingPoint
+            }
         case .bottomTrailingPoint:
             let value = (size.width + size.height) / 2
             bottomTrailingPoint = CGPoint(x: previousBottomTrailingPoint.x + value, y: previousBottomTrailingPoint.y + value)
             bottomLeadingPoint = CGPoint(x: previousBottomLeadingPoint.x, y: previousBottomLeadingPoint.y + value)
             topTrailingPoint = CGPoint(x: previousTopTrailingPoint.x + value, y: previousTopTrailingPoint.y)
+            
+            if CheckDraggableUseCase(point1: bottomTrailingPoint, point2: topLeadingPoint, minDiff: minDiff, originRect: originRect).execute() {
+                self.bottomTrailingPoint = bottomTrailingPoint
+                self.bottomLeadingPoint = bottomLeadingPoint
+                self.topTrailingPoint = topTrailingPoint
+            }
         }
     }
     
