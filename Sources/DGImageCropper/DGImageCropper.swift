15086da94d8dfbd5a457f87746c2d7d76a87114e
Loopy <loopy@heyratel.com>
2025-06-09 14:06:47 +0900

    chore: 다른 케이스도 확인

--- Sources/DGImageCropper/DGImageCropper.swift
+++ Sources/DGImageCropper/DGImageCropper.swift
@@ -146,7 +146,7 @@ public struct DGImageCropper: View {
 #if DEBUG
 private struct DGImageCropperPreview: View {
     @State private var croppedImage: UIImage?
-    let model: ImageCropperModel = .init(image: .init(contentsOfFile: Bundle.module.path(forResource: "IMG_4815", ofType: "jpeg")!)!)
+    let model: ImageCropperModel = .init(image: .init(contentsOfFile: Bundle.module.path(forResource: "IMG_8048", ofType: "PNG")!)!)
     
     var body: some View {
         VStack {
