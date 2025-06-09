c9886eb8536881dcd6767f0463d0be709f46734b
Loopy <loopy@heyratel.com>
2025-06-09 14:01:46 +0900

    chore: find error case

--- Sources/DGImageCropper/DGImageCropper.swift
+++ Sources/DGImageCropper/DGImageCropper.swift
@@ -146,7 +146,7 @@ public struct DGImageCropper: View {
 #if DEBUG
 private struct DGImageCropperPreview: View {
     @State private var croppedImage: UIImage?
-    let model: ImageCropperModel = .init(image: .init(contentsOfFile: Bundle.module.path(forResource: "error_case_image", ofType: "jpg")!)!)
+    let model: ImageCropperModel = .init(image: .init(contentsOfFile: Bundle.module.path(forResource: "IMG_4815", ofType: "jpeg")!)!)
     
     var body: some View {
         VStack {
